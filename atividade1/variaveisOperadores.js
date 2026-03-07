1473c73bba19c477df22868b2368ca2c5216201b
oBrittop <gustavobrito2416@gmail.com>
2026-03-07 17:35:41 -0300

    Correção atividade 1, Completa

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -6,7 +6,7 @@ let resultadoSubtracao = numero1 - numero2
 let resultadoMultiplicacao = numero1 * numero2
 let resultadoDivisao = numero1 / numero2
 
-// Mostrando o resultado no console (aperte F12 no navegador para ver)
+
 console.log("O resultado da soma em JS é:", resultadoSoma)
 console.log("O resultado da subtração em JS é:", resultadoSubtracao)
 console.log("O resultado da multiplicação em JS é:", resultadoMultiplicacao)
