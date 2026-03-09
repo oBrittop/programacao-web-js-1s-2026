a8db3071c8a92f5988166c303572af9b9732bfe6
oBrittop <gustavobrito2416@gmail.com>
2026-03-09 20:09:38 -0300

    Atualizando e pasta nova

---
+++ atividade1/js_basico/variabiwsTipos.js
@@ -0,0 +1,29 @@
+//Strings
+let nome = "Gustavo"
+let sobrenome = "Brito"
+let templateString = `O meu nome e ${nome} ${sobrenome}`
+console.log(templateString)
+
+// Number
+let a = 2
+let b = 5.14
+let c = -3.14
+const PI = 3.14
+let potencia = a **c
+
+console.log(typeof a)
+console.log(typeof c)
+console.log(potencia)
+// console.log(typeof )
+// console.log(typeof )
+
+//Bolean
+let verdadeiro = true
+let falso = false
+console.log(typeof verdadeiro)
+
+//Operadores Logicos
+let e = verdadeiro && falso
+let ou = verdadeiro || falso
+console.log(e)
+console.log(ou)
