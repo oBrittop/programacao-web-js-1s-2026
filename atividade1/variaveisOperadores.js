be0a56e98b0380eb95ab0bb1ba6cdc90065d6840
oBrittop <gustavobrito2416@gmail.com>
2026-03-09 19:37:26 -0300

    modify: mudança função soma

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -7,7 +7,7 @@ let resultadoMultiplicacao = numero1 * numero2
 let resultadoDivisao = numero1 / numero2
 
 
-console.log("O resultado da soma em JS é:", resultadoSoma)
+console.log(`O resultadp de ${numero1} + ${numero2} e ${resultadoSoma}`)
 console.log("O resultado da subtração em JS é:", resultadoSubtracao)
 console.log("O resultado da multiplicação em JS é:", resultadoMultiplicacao)
 console.log("O resultado da divisão em JS é:", resultadoDivisao)
